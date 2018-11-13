dced210081b855d50a3f5bd07e0dfd523000329c
NawaF11Prince <44409762+NawaF11Prince@users.noreply.github.com>
2018-11-13 17:06:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ client.user.setGame(`صيآنة`,"http://twitch.tv/S-F")
 
 client.on('ready', function(){    
     var ms = 40000 ;    
-    var setGame = [` Abo0d_ ❤😜: `," #Server Mr_A7maDxD"];    
+    var setGame = [` Abo0d_ ❤😜: `," The Best :D "];    
     var i = -1;    
     var j = 0;    
     setInterval(function (){    
